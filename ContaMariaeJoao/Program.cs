0df43a1ec9f00377efc602fd3bc8efa653b0c619
Emerson Marques Pedro <emersonmp@gmail.com>
2024-04-11 20:25:23 -0300

    Commit inicial

---
+++ ContaMariaeJoao/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class ContaBancaria
+{
+    public string Titular;
+    public double Saldo;
+
+    public ContaBancaria(string titular, double saldoInicial)
+    {
+        Titular = titular;
+        Saldo = saldoInicial;
+    }
+
+    public void Depositar(double valor)
+    {
+        Saldo += valor;
+        Console.WriteLine($"Depósito de {valor} realizado com sucesso. Novo saldo: {Saldo}");
+    }
+
+    public void Sacar(double valor)
+    {
+        if (valor > Saldo)
+        {
+            Console.WriteLine("Saldo insuficiente");
+        }
+        else
+        {
+            Saldo -= valor;
+            Console.WriteLine($"Saque de {valor} realizado com sucesso. Novo saldo: {Saldo}");
+        }
+    }
+
+    public void ExibirSaldo()
+    {
+        Console.WriteLine($"Saldo atual da conta de {Titular}: {Saldo}");
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando contas bancárias
+        ContaBancaria contaJoao = new ContaBancaria("João", 1000);
+        ContaBancaria contaMaria = new ContaBancaria("Maria", 2000);
+
+        // Realizando operações
+        contaJoao.Depositar(500);
+        contaJoao.Sacar(200);
+
+        contaMaria.Sacar(300);
+        contaJoao.Depositar(300);
+
+        // Exibindo saldos
+        contaJoao.ExibirSaldo();
+        contaMaria.ExibirSaldo();
+    }
+}
